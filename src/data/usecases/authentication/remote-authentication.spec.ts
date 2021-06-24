b0683c59901b08dd78e1a2d647385939f8c81d86
Patrick Coutinho <patrick@istyle.com.br>
2021-06-23 22:28:38 -0300

    refactor: updated code format

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,9 +1,9 @@
-import { HttpPostClientSpy } from "../../mocks/http-client";
-import { RemoteAuthentication } from "./remote-authentication";
+import { HttpPostClientSpy } from '../../mocks/http-client';
+import { RemoteAuthentication } from './remote-authentication';
 
-describe("RemoteAuthetication", () => {
-  test("Should call HttpPostClient with correct URL", () => {
-    const url = "any";
+describe('RemoteAuthetication', () => {
+  test('Should call HttpPostClient with correct URL', () => {
+    const url = 'any';
     const httpPostClient = new HttpPostClientSpy();
     const sut = new RemoteAuthentication(url, httpPostClient);
     sut.auth();
