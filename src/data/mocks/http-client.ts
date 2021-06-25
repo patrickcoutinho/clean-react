0609693ac3965bdbfe518ec73f5fa99dd314145e
Patrick Coutinho <patrick@istyle.com.br>
2021-06-24 21:22:44 -0300

    feat: ensure Remote Authentication calss HttpPostClient with correct body

--- src/data/mocks/http-client.ts
+++ src/data/mocks/http-client.ts
@@ -3,8 +3,11 @@ import { HttpPostClient, HttpPostParams } from '../protocols/http/http-post.clie
 export default class HttpPostClientSpy implements HttpPostClient {
   url?: string;
 
+  body?: object;
+
   async post(params: HttpPostParams): Promise<void> {
     this.url = params.url;
+    this.body = params.body;
     return Promise.resolve();
   }
 }
