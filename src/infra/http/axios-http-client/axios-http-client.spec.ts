f8a20f66ac16477f22f7d6ef378724c34f583623
Patrick Coutinho <patrick@istyle.com.br>
2021-06-30 18:43:47 -0300

    refactor: move axios mock to a separate file

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -1,41 +1,39 @@
-import { HttpPostParams } from '@/data/protocols/http';
+import { axiosMock } from '@/infra/mocks';
 import axios from 'axios';
-import faker from 'faker';
+import { postRequestMock } from '@/data/mocks';
 import { AxiosHttpClient } from './axios-http-client';
 
 jest.mock('axios');
 
-const mockedAxios = axios as jest.Mocked<typeof axios>;
-const mockedAxiosResult = {
-  data: faker.random.objectElement(),
-  status: faker.datatype.number(),
+type SubjectTypes = {
+  subject: AxiosHttpClient
+  mockedAxios: jest.Mocked<typeof axios>
 };
 
-mockedAxios.post.mockResolvedValue(mockedAxiosResult);
+const makeSubject = (): SubjectTypes => {
+  const subject = new AxiosHttpClient();
+  const mockedAxios = axiosMock();
 
-const makeSubject = ():AxiosHttpClient => new AxiosHttpClient();
-
-const mockPostRequest = ():HttpPostParams<any> => ({
-  url: faker.internet.url(),
-  body: faker.random.objectElement(),
-});
+  return {
+    subject,
+    mockedAxios,
+  };
+};
 
 describe('AxiosHttpClient', () => {
   test('Should calls axios with correct values', async () => {
-    const request = mockPostRequest();
-    const subject = makeSubject();
+    const request = postRequestMock();
+    const { subject, mockedAxios } = makeSubject();
+
     await subject.post(request);
 
     expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body);
   });
 
   test('Should calls axios and returns correct statusCode and body', async () => {
-    const subject = makeSubject();
-    const httpResponse = await subject.post(mockPostRequest());
+    const { subject, mockedAxios } = makeSubject();
+    const promise = subject.post(postRequestMock());
 
-    expect(httpResponse).toEqual({
-      statusCode: mockedAxiosResult.status,
-      body: mockedAxiosResult.data,
-    });
+    expect(promise).toEqual(mockedAxios.post.mock.results[0].value);
   });
 });
