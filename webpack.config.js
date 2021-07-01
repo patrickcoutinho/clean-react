2ca705705c6b71cd07023171e69fbd21e02b0f68
Patrick Coutinho <patrick@istyle.com.br>
2021-07-01 19:14:37 -0300

    chore: fixed webpack configs

--- webpack.config.js
+++ webpack.config.js
@@ -47,8 +47,20 @@ module.exports = {
     historyApiFallback: true
   },
   externals: {
-    react: 'React',
-    'react-dom': 'ReactDom'
+    react: {
+      root: 'React',
+      commonjs2: 'react',
+      commonjs: 'react',
+      amd: 'react',
+      umd: 'react',
+    },
+    'react-dom': {
+      root: 'ReactDOM',
+      commonjs2: 'react-dom',
+      commonjs: 'react-dom',
+      amd: 'react-dom',
+      umd: 'react-dom',
+    },
   },
   plugins: [
     new CleanWebpackPlugin()
