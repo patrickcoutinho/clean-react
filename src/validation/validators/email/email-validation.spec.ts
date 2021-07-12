aed43c7805d36488d6631bd3ffff1d81fb5db948
Patrick Coutinho <patrick@istyle.com.br>
2021-07-12 20:34:58 -0300

    test: ensure EmailValidation returns falsy if email is valid

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -1,11 +1,21 @@
 import { InvalidFieldError } from '@/validation/errors';
+import faker from 'faker';
 import { EmailValidation } from './email-validation';
+
+const makeSubject = () => new EmailValidation(faker.random.word());
 
 describe('EmailValidation', () => {
   test('Should return error if email is invalid', () => {
-    const subject = new EmailValidation('enail');
-    const error = subject.validate('');
+    const subject = makeSubject();
+    const error = subject.validate(faker.random.word());
 
     expect(error).toEqual(new InvalidFieldError());
+  });
+
+  test('Should return falsy if email is valid', () => {
+    const subject = makeSubject();
+    const error = subject.validate(faker.internet.email());
+
+    expect(error).toBeFalsy();
   });
 });
