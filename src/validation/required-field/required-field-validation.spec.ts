c058d0ee6ddb643bfea258047bb518a5b28c6b5e
Patrick Coutinho <patrick@istyle.com.br>
2021-07-12 19:57:33 -0300

    refactor: move subject creation to a factory method

--- src/validation/required-field/required-field-validation.spec.ts
+++ src/validation/required-field/required-field-validation.spec.ts
@@ -2,16 +2,19 @@ import faker from 'faker';
 import { RequiredFieldError } from '../errors';
 import { RequiredFieldValidation } from './required-field-validation';
 
+const makeSubject = ():
+RequiredFieldValidation => new RequiredFieldValidation(faker.database.column());
+
 describe('RequiredFieldValidation', () => {
   test('Should return error if field is empty', () => {
-    const subject = new RequiredFieldValidation('email');
+    const subject = makeSubject();
     const error = subject.validate('');
 
     expect(error).toEqual(new RequiredFieldError());
   });
 
   test('Should return falsy if field is not empty', () => {
-    const subject = new RequiredFieldValidation('email');
+    const subject = makeSubject();
     const error = subject.validate(faker.random.word());
 
     expect(error).toBeFalsy();
