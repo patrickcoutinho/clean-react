c7c29de2fc41628be50be854ffd4b2e6d4a88d0d
Patrick Coutinho <patrick@istyle.com.br>
2021-07-12 21:07:21 -0300

    refactor: move subject creation to a factory method

--- src/validation/validators/min-length/min-length-validation.spec.ts
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -1,18 +1,25 @@
 import { InvalidFieldError } from '@/validation/errors';
+import faker from 'faker';
 import { MinLengthValidation } from './min-length-validation';
+
+const makeSubject = (minLength: number = 6):
+MinLengthValidation => new MinLengthValidation(faker.database.column(), minLength);
 
 describe('MinLengthValidation', () => {
   test('Should return error if value is less than 6 chars', () => {
-    const subject = new MinLengthValidation('field', 6);
-    const error = subject.validate('abc');
+    const subject = makeSubject();
+    const error = subject.validate(faker.random.alphaNumeric(3));
 
     expect(error).toEqual(new InvalidFieldError());
   });
 
   test('Should return falsy if value is valid', () => {
-    const subject = new MinLengthValidation('field', 6);
-    const error = subject.validate('abcdef');
+    const subject = makeSubject();
 
+    let error = subject.validate(faker.random.alphaNumeric(6));
+    expect(error).toBeFalsy();
+
+    error = subject.validate(faker.random.alphaNumeric(7));
     expect(error).toBeFalsy();
   });
 });
