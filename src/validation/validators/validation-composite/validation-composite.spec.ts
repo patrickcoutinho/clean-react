6a872aa16feccd49ab80422ccce1c74d96090ada
Patrick Coutinho <patrick@istyle.com.br>
2021-07-13 19:07:58 -0300

    test: ensure ValidationComposite returns the first error found

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -2,7 +2,7 @@ import { FieldValidationSpy } from '../mocks';
 import { ValidationComposite } from './validation-composite';
 
 describe('ValidationComposite', () => {
-  test('Should return error if any validation fails ', () => {
+  test('Should return the first error found if any validation fails ', () => {
     const fieldValidationSpy = new FieldValidationSpy('any_field');
     fieldValidationSpy.error = new Error('first_error_message');
     const fieldValidationSpy2 = new FieldValidationSpy('any_field');
