4c29203ccb29fe340f8a3f7e82adc331f65074f7
Patrick Coutinho <patrick@istyle.com.br>
2021-07-14 21:06:59 -0300

    chore: update jest config to ignore main layer factories

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
     '<rootDir>/src/**/*.{ts,tsx}',
+    '!<rootDir>/src/main/factories/**/*.{ts,tsx}',
     '!**/*.d.ts'
   ],
   coverageDirectory: 'coverage',
