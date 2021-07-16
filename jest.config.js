16d092b0eb5beb70d7f0dbc525112e2994733978
Patrick Coutinho <patrick@istyle.com.br>
2021-07-16 20:56:52 -0300

    chore: change to jest ignore index with exports

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,12 @@ module.exports = {
     '<rootDir>/src/**/*.{ts,tsx}',
     '!<rootDir>/src/main/factories/**/*.{ts,tsx}',
     '!<rootDir>/src/presentation/components/router/**/*',
+    '!<rootDir>/src/domain/**/index.ts',
+    '!<rootDir>/src/infra/**/index.ts',
+    '!<rootDir>/src/main/**/index.ts',
+    '!<rootDir>/src/presentation/**/index.ts',
+    '!<rootDir>/src/main/**/*',
+    '!<rootDir>/src/validation/**/index.ts',
     '!**/*.d.ts'
   ],
   coverageDirectory: 'coverage',
