4bc5870a84234c0bc7f8dbc280bb4afb201431e6
Patrick Coutinho <patrick@istyle.com.br>
2021-07-17 12:25:54 -0300

    refactor: move subject creation to a factory method

--- src/infra/storage/local-storage-adapter.spec.ts
+++ src/infra/storage/local-storage-adapter.spec.ts
@@ -2,13 +2,15 @@ import faker from 'faker';
 import 'jest-localstorage-mock';
 import { LocalStorageAdapter } from './local-storage-adapter';
 
+const makeSubject = (): LocalStorageAdapter => new LocalStorageAdapter();
+
 describe('LocalStorageAdapter', () => {
   beforeEach(() => {
     localStorage.clear();
   });
 
   test('Should call window.localStorage with correct values', async () => {
-    const subject = new LocalStorageAdapter();
+    const subject = makeSubject();
     const key = faker.database.column();
     const value = faker.random.words();
 
