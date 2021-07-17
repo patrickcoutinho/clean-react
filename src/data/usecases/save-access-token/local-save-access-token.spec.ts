c5a78b9b6afa04c5562ed482207fe2ab57c17a3d
Patrick Coutinho <patrick@istyle.com.br>
2021-07-17 11:54:53 -0300

    refactor: move subject creation to a factory method

--- src/data/usecases/save-access-token/local-save-access-token.spec.ts
+++ src/data/usecases/save-access-token/local-save-access-token.spec.ts
@@ -2,11 +2,22 @@ import { SetSorageSpy } from '@/data/mocks';
 import faker from 'faker';
 import { LocalSaveAccessToken } from './local-save-access-token';
 
+type SubjectTypes = {
+  subject: LocalSaveAccessToken
+  setStorageSpy: SetSorageSpy
+};
+
+const makeSubject = (): SubjectTypes => {
+  const setStorageSpy = new SetSorageSpy();
+  const subject = new LocalSaveAccessToken(setStorageSpy);
+
+  return { subject, setStorageSpy };
+};
+
 describe('LocalSaveAccessToken', () => {
   test('Should call SetStorage with corret value', async () => {
-    const setStorageSpy = new SetSorageSpy();
-    const subject = new LocalSaveAccessToken(setStorageSpy);
     const accessToken = faker.datatype.uuid();
+    const { subject, setStorageSpy } = makeSubject();
 
     await subject.save(accessToken);
 
