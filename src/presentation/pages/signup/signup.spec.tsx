eaf0e87810ebbc1ca3ef4ed8f4b847e89ca8e52d
Patrick Coutinho <patrick@istyle.com.br>
2021-07-21 21:42:56 -0300

    test: ensure SignUp show name error if validation fails

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -1,24 +1,37 @@
 import React from 'react';
-import { render, RenderResult } from '@testing-library/react';
+import { cleanup, render, RenderResult } from '@testing-library/react';
 import { formHelper } from '@/presentation/test-helpers';
+import { ValidationStub } from '@/presentation/mocks';
+import faker from 'faker';
 import SignUp from './signup';
 
 type SubjectTypes = {
   subject: RenderResult
 };
 
-const makeSubject = (): SubjectTypes => {
+type SubjectParams = {
+  validationErrror: string
+};
+
+const makeSubject = (params?: SubjectParams): SubjectTypes => {
+  const validationStub = new ValidationStub();
+  validationStub.errorMessage = params?.validationErrror;
   const subject = render(
-    <SignUp />,
+    <SignUp
+      validation={validationStub}
+    />,
   );
 
   return { subject };
 };
 
+const validationError = faker.random.words();
+
 describe('SignUp Page', () => {
+  afterEach(cleanup);
+
   test('Should start with initial state', () => {
-    const validationError = 'Campo obrigatório';
-    const { subject } = makeSubject();
+    const { subject } = makeSubject({ validationErrror: validationError });
 
     formHelper.testChildCount(subject, 'error-wrapper', 0);
     formHelper.testButtonIsDisabled(subject, 'submit', true);
@@ -27,5 +40,14 @@ describe('SignUp Page', () => {
     formHelper.testStatusForField(subject, 'email', validationError);
     formHelper.testStatusForField(subject, 'password', validationError);
     formHelper.testStatusForField(subject, 'passwordConfirmation', validationError);
+  });
+
+  test('Should show name error if validation fails', () => {
+    const { subject } = makeSubject({
+      validationErrror: validationError,
+    });
+
+    formHelper.populateInput(subject, 'name');
+    formHelper.testStatusForField(subject, 'name', validationError);
   });
 });
